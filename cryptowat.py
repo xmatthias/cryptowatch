88dfa03352445296fd119206ebd579388263d04c
Matthias Voppichler <matthias.voppichler@gmail.com>
2017-12-30 14:54:34 +0100

    Additional methods

--- cryptowat.py
+++ cryptowat.py
@@ -14,22 +14,38 @@ API_URL = "https://api.cryptowat.ch/"
 allowance = {}
 
 
-def request(url):
+# source: https://stackoverflow.com/questions/6578986/how-to-convert-json-data-into-a-python-object
+def _json_object_hook(dat):
+    """Json hook to convert json object to named tuple
+    DO NOT CALL DIRECTLY"""
+    return namedtuple('X', dat.keys())(*dat.values())
+
+
+def json2obj(data):
+    """Convert json object to NamedTuple"""
+    return json.loads(data, object_hook=_json_object_hook)
+
+
+def request(url, convert=True):
+    """Requests to api and return python object"""
+    # pylint: disable=W0603
     global allowance
     req = requests.get(url)
-    res = req.json()
-    x = json.loads(req.text, object_hook=lambda d: namedtuple(
-        'X', d.keys())(*d.values()))
-    print(dir(x.result))
-    allowance = res["allowance"]
+    if convert:
+        res = json2obj(req.text).result
+        allowance = res.allowance
+    else:
+        allowance = json2obj(json.dumps(req.json()["allowance"]))
+        res = req.json()["result"]
+
     printallowance()
-    return res["result"]
+    return res
 
 
 def printallowance():
     """prints remaining allowance"""
-    rem = int(allowance["remaining"])
-    cost = int(allowance["cost"])
+    rem = int(allowance.remaining)
+    cost = int(allowance.cost)
     avg_rem = rem / cost
     print(Fore.YELLOW + "Allowance:" + Fore.LIGHTWHITE_EX)
     print("Cost:", cost)
@@ -42,9 +58,9 @@ def printassets(assets):
     """ Prints available assets - accepts asset json object """
     max = 25
     for ass in assets:
-        #print(ass["symbol"], ass["name"], ass["symbol"], ass["route"])
+        #print(ass.symbol, ass.name, ass.symbol, ass.route)
         print(
-            f'{ass["name"]: <{max}} {ass["symbol"]: <7} {ass["route"]}')
+            f'{ass.name: <{max}} {ass.symbol: <7} {ass.route}')
 
 
 def listassets():
@@ -55,8 +71,8 @@ def listassets():
 
 def printasset(asset):
     """prints asset details - accepts assset list"""
-    for a in asset["markets"]["base"]:
-        print(f'{a["exchange"]: <10} {a["pair"]: <20} {a["active"]} {a["route"]}')
+    for a in asset.markets.base:
+        print(f'{a.exchange: <10} {a.pair: <20} {a.active} {a.route}')
 
 
 def getasset(asset):
@@ -68,7 +84,7 @@ def getasset(asset):
 def printpairs(pairs):
     """prints pairs"""
     for pair in pairs:
-        print(f'{pair["base"]["name"]:20} {pair["symbol"]: <30} {pair["route"]}')
+        print(f'{pair.base.name:20} {pair.symbol: <30} {pair.route}')
 
 
 def listpairs():
@@ -79,10 +95,10 @@ def listpairs():
 
 def printpair(pair):
     """prints pair detail view"""
-    print(f'{pair["symbol"]} {pair["base"]["name"]} {pair["quote"]["name"]}')
+    print(f'{pair.symbol} {pair.base.name} {pair.quote.name}')
     print("Markets:")
-    for market in pair["markets"]:
-        print(f'{market["exchange"]: <10} {market["active"]: <5}')
+    for market in pair.markets:
+        print(f'{market.exchange: <10} {market.active: <5}')
 
 
 def getpair(pair):
@@ -93,12 +109,12 @@ def getpair(pair):
 
 def printexchanges(exchanges):
     """ Prints available exchanges - accepts exchange json object """
-    #df = json_normalize(exchanges)
-    #print(df.loc[df.active == 1])
+    # df = json_normalize(exchanges)
+    # print(df.loc[df.active == 1])
     max = 20
     for ex in exchanges:
-        print(f'{ex["name"]: <{max}} {ex["symbol"]: <{max}}'
-              f' {ex["route"]: <{max}}')
+        print(f'{ex.name: <{max}} {ex.symbol: <{max}}'
+              f' {ex.route: <{max}}')
 
 
 def listexchanges():
@@ -108,13 +124,17 @@ def listexchanges():
 
 
 def printmarkets(markets):
-    for m in markets:
-        print(m["exchange"], m["pair"],  m["route"])
+    """Print markets sorted by exchange"""
+    for m in sorted(markets, key=lambda m: (m.exchange, m.pair)):
+        print(f'{m.exchange: <15} {m.pair: <25} {m.route}')
 
 
-def getmarkets(exchange):
-    """Gets markets for selected exchange"""
-    markets = request(API_URL + "markets/" + exchange)
+def listmarkets(exchange=None):
+    """returns a namedtuple with all available markets"""
+    if exchange:
+        markets = request(API_URL + "markets/" + exchange)
+    else:
+        markets = request(API_URL + "markets")
     return markets
 
 
@@ -124,12 +144,24 @@ def getmarketpair(exchange, pair):
     return p
 
 
+def printprices(prices):
+    """prints all prices"""
+    for key, value in sorted(prices.items()):
+        print(f'{key: <40} {value}')
+
+
+def getallprices():
+    """ get all prices in a namedtupel object"""
+    prices = request(API_URL + "markets/prices", False)
+    return prices
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--listassets", dest="listassets",
                         action="store_true", required=False,
                         help="Get asset index and exit")
-    parser.add_argument("-a", "--assets", dest="assets", nargs="*",
+    parser.add_argument("-a", "--asset", dest="asset",
                         required=False,
                         help="Get detail info about Asset")
 
@@ -142,20 +174,27 @@ def main():
 
     parser.add_argument("--listexchanges", dest="listexchanges",
                         action="store_true", required=False,
-                        help="Get exchange list and exit")
+                        help="Get exchange index and exit")
 
-    parser.add_argument("-m", "--market", dest="market", required=False)
+    parser.add_argument("--listmarkets", dest="listmarkets",
+                        action="store_true", required=False,
+                        help="Get markets index and exit")
+
+    parser.add_argument("-m", "--market", dest="market", required=False,
+                        help="Get available markets / pairs for this exchange")
     parser.add_argument("-p", "--pairs", dest="pairs", nargs="*",
                         help="Pairs to monitor")
+    parser.add_argument("--getallprices", dest="getallprices",
+                        action="store_true", required=False,
+                        help="Get all prices exit")
     args = parser.parse_args()
 
     if args.listassets:
         printassets(listassets())
         return
 
-    if args.assets:
-        for asset in args.assets:
-            printasset(getasset(asset))
+    if args.asset:
+        printasset(getasset(args.asset))
 
     if args.listpairs:
         printpairs(listpairs())
@@ -167,16 +206,19 @@ def main():
         printexchanges(listexchanges())
         return
 
+    if args.listmarkets:
+        printmarkets(listmarkets())
+
     if args.market and not args.pairs:
-        markets = getmarkets(args.market)
+        markets = listmarkets(args.market)
         printmarkets(markets)
 
     if args.market and args.pairs:
         for pair in args.pairs:
             print(getmarketpair(args.market, pair))
-    return
-    res = requests.get(API_URL + "markets")
 
+    if args.getallprices:
+        printprices(getallprices())
 
 if __name__ == "__main__":
     pd.options.display.width = 120
