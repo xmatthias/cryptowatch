545e2ff01cf61baec043964acd40b5397ef243d5
Matthias Voppichler <matthias.voppichler@gmail.com>
2018-01-07 20:29:52 +0100

    Fix quotes - add pandas to setup-requirement

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 """setup module"""
 from setuptools import setup, find_packages
 
-setup(name='cryptowatch',
-      version='0.1',
-      description='cryptowat.ch cryptocurrency Exchange API Pyhon Client',
-      url='http://github.com/xmatthias/cryptowatch',
-      author='Matthias Voppichler',
-      author_email='xmatthias@outlook.com',
-      license='Apache License 2.0',
-      scripts=['cryptowat.py'],
+setup(name="cryptowatch",
+      version="0.1",
+      description="cryptowat.ch cryptocurrency Exchange API Pyhon Client",
+      url="http://github.com/xmatthias/cryptowatch",
+      author="Matthias Voppichler",
+      author_email="xmatthias@outlook.com",
+      license="Apache License 2.0",
+      scripts=["cryptowat.py"],
       # packages=find_packages(),
       packages=["cryptowatch"],
       install_requires=[
-          'requests',
-          'colorama'
+          "requests",
+          "colorama",
+          "pandas"
       ],
       zip_safe=False)
