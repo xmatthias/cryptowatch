91bd97bc57354f4a6236158c9230e658c27a9379
Matthias Voppichler <matthias.voppichler@gmail.com>
2018-01-07 20:41:08 +0100

    improve versioning

--- cryptowatch/__init__.py
+++ cryptowatch/__init__.py
@@ -3,3 +3,4 @@
 import cryptowatch.crypto_info
 from cryptowatch.CryptoMarket import CryptoMarket
 from cryptowatch.crypto_tools import allowance, printallowance
+from cryptowatch.version import __version__
